ebbe13b6e6c075ccced1f64e3946677f88e926a5
dibster <dave.ashenhurst@googlemail.com>
2013-09-19 12:53:51 +0100

    base project with angular seed

---
+++ kaboodleMobile/app/js/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('MyCtrl1', [function() {
+                var a = 1;
+  }])
+  .controller('MyCtrl2', [function() {
+
+  }]);
