7e628311e118a5235630fe11a408cce5c3496986
dibster <dave.ashenhurst@googlemail.com>
2013-09-21 14:49:09 +0100

    Node App

--- kaboodleMobile/app/js/app.js
+++ kaboodleMobile/app/js/app.js
@@ -1,8 +1,8 @@
 'use strict';
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters',
-                         'myApp.services',
-                         'myApp.directives',
+angular.module('kmApp', ['kmApp.filters',
+                         'kmApp.services',
+                         'kmApp.directives',
                          'kaboodle.News.controllers',
                          'kaboodle.Events.controllers']).
   config(['$routeProvider', function($routeProvider) {
